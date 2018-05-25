3956f597c3741aed4678400692fcd68bebfa0a97
srisharan kenam <36810338+sharan59@users.noreply.github.com>
2018-05-25 11:51:48 +0530

    Add files via upload

---
+++ dist/main.bundle.js
@@ -0,0 +1,344 @@
+webpackJsonp(["main"],{
+
+/***/ "../../../../../src/$$_lazy_route_resource lazy recursive":
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncatched exception popping up in devtools
+	return Promise.resolve().then(function() {
+		throw new Error("Cannot find module '" + req + "'.");
+	});
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = "../../../../../src/$$_lazy_route_resource lazy recursive";
+
+/***/ }),
+
+/***/ "../../../../../src/app/addus/addus.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/addus/addus.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<p>\n  addus works!\n</p>\n"
+
+/***/ }),
+
+/***/ "../../../../../src/app/addus/addus.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AddusComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var AddusComponent = (function () {
+    function AddusComponent() {
+    }
+    AddusComponent.prototype.ngOnInit = function () {
+    };
+    AddusComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+            selector: 'app-addus',
+            template: __webpack_require__("../../../../../src/app/addus/addus.component.html"),
+            styles: [__webpack_require__("../../../../../src/app/addus/addus.component.css")]
+        }),
+        __metadata("design:paramtypes", [])
+    ], AddusComponent);
+    return AddusComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "body {\r\n  padding-top: 30px;\r\n}\r\n#next{\r\n    float: left;\r\n}\r\ntable {\r\n    font-family: arial, sans-serif;\r\n    border-collapse: collapse;\r\n    width: 100%;\r\n    \r\n}\r\n\r\ntd, th {\r\n    border: 2px solid #dddddd;\r\n    text-align: left;\r\n    padding: 10px;\r\n}\r\n\r\ntr:nth-child(even) {\r\n    background-color: #dddddd;\r\n}\r\n#close{\r\n  background-color: #4CAF50; /* Green */\r\n  border: none;\r\n  color: white;\r\n  padding: 15px 32px;\r\n  text-align: center;\r\n  float : right;\r\n  text-decoration: none;\r\n  /* display: inline-block; */\r\n  cursor: pointer;\r\n  font-size: 10px;\r\n\r\n}\r\n\r\n#one{\r\n  background-color: #4CAF50; /* Green */\r\n  border: none;\r\n  color: white;\r\n  text-align: center;\r\n  float : right;\r\n  text-decoration: none;\r\n  /* display: inline-block; */\r\n  cursor: pointer;\r\n  font-size: 20px;\r\n  margin-top: -50px;\r\n\r\n}\r\n#two {\r\nbackground-color: #4CAF50; /* Green */\r\nborder: none;\r\ncolor: white;\r\npadding: 15px 32px;\r\ntext-align: center;\r\ntext-decoration: none;\r\n\r\n/* float: right; */\r\n/* display: inline-block; */\r\nmargin: 4px 2px;\r\ncursor: pointer;\r\nfont-size: 10px;\r\n}\r\n.textbox{\r\n  height:40px;\r\n  width:100%;\r\n  border-radius:3px;\r\n  border:rgba(0,0,0,.3) 2px solid;\r\n  box-sizing:border-box;\r\n  padding:5px;\r\n  margin-bottom:20px;\r\n}\r\n#main {\r\n  background-color: #1565c0; /* Green */\r\n  border: none;\r\n  color: white;\r\n  padding: 15px 32px;\r\n  text-align: center;\r\n  text-decoration: none;\r\n  display: inline-block;\r\n  margin: 3px 2px;\r\n  cursor: pointer;\r\n  font-size: 10px;\r\n  }\r\n  ", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<body>\n\n  <div class=\"container\">\n    <h2>User Directory</h2>\n    <!-- Trigger the modal with a button -->\n    <button type=\"button\" class=\"btn btn-info btn-lg\" id=\"one\" data-toggle=\"modal\" data-target=\"#myModal\">Add New User</button>\n  \n    <!-- Modal -->\n    <div class=\"modal fade\" id=\"myModal\" role=\"dialog\">\n      <div class=\"modal-dialog modal-sm\">\n        <div class=\"modal-content\">\n          <div class=\"modal-header\">\n           \n            <h4 class=\"modal-title\">Add New User</h4>\n          </div>\n          <div class=\"modal-body\">\n            <form #nuser='ngForm' (ngSubmit)=transfer(nuser.value)>\n              <input type=\"text\" class=\"textbox\" placeholder=\"UserName\" name=\"username\" ngModel>\n              <input type=\"text\" class=\"textbox\" placeholder=\"Address\" name=\"address\" ngModel>\n              <input type=\"text\" class=\"textbox\" placeholder=\"Contact\" name=\"contact\" ngModel>\n              <input type=\"text\" class=\"textbox\" placeholder=\"Email\" name=\"email\" ngModel>\n              <input type=\"submit\" id=\"main\" value=\"AddUser\">\n              \n              </form>\n          </div>\n          <div class=\"modal-footer\">\n            <button type=\"button\" id=\"close\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button>\n          </div>\n        </div>\n      </div>\n    </div>\n  </div>\n  \n  </body>\n  \n\n  <table>\n    <tr>\n      <th>USERNAME </th>\n      <th>ADDRESS </th>  \n      <th>EMAIL </th>\n      <th>CONTACT </th>\n    </tr>\n    <tr *ngFor=\"let reg of adata ; let in=index\">  \n      <td>{{reg.username}}</td>\n    <td>{{reg.address}}</td>\n    <td>{{reg.email}}</td>\n    <td>{{reg.contact}} <button id=\"two\" (click)='deleteData(reg.username,in)' >Delete</button>\n      <button id=\"two\" (click)='editData(reg,in)' >Edit</button></td> \n    </tr> \n    </table>\n\n    <div *ngIf=\"update\" >\n    <input type=\"text\" placeholder=\"UserName\" name=\"username\" [(ngModel)]=datatobeedited.name>\n              <input type=\"text\" placeholder=\"Address\" name=\"address\" [(ngModel)]=datatobeedited.address ngModel>\n              <input type=\"text\" placeholder=\"Contact\" name=\"contact\" [(ngModel)]=datatobeedited.contact ngModel>\n              <input type=\"text\" placeholder=\"Email\" name=\"email\" [(ngModel)]=datatobeedited.email ngModel>\n              <button (click)='saveUpdate(datatobeedited)' >save</button>\n            </div>\n"
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_rxjs_add_operator_map__ = __webpack_require__("../../../../rxjs/_esm5/add/operator/map.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_rxjs_add_operator_catch__ = __webpack_require__("../../../../rxjs/_esm5/add/operator/catch.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_http__ = __webpack_require__("../../../http/esm5/http.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__data_service__ = __webpack_require__("../../../../../src/app/data.service.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+
+
+var AppComponent = (function () {
+    function AppComponent(_http, test) {
+        var _this = this;
+        this._http = _http;
+        this.test = test;
+        this.update = false;
+        this.test.getallData().subscribe(function (testData) { return _this.adata = testData; });
+    }
+    AppComponent.prototype.ngOnInit = function () {
+    };
+    AppComponent.prototype.transfer = function (a) {
+        this.username = a.username;
+        this.address = a.address;
+        this.contact = a.contact;
+        this.email = a.email;
+        console.log(this.username + this.address + this.contact + this.email);
+        console.log(a);
+        var headers = new __WEBPACK_IMPORTED_MODULE_3__angular_http__["a" /* Headers */]();
+        headers.append('Content-Type', 'application/json');
+        var options = new __WEBPACK_IMPORTED_MODULE_3__angular_http__["d" /* RequestOptions */]({ headers: headers });
+        this._http.post('/api/adduser', a, options)
+            .subscribe(function (data) {
+            alert('Registered Successfully');
+        }, function (error) {
+            console.log(JSON.stringify(error.json()));
+        });
+    };
+    AppComponent.prototype.deleteData = function (username, x) {
+        var _this = this;
+        console.log(username);
+        this.test.deleteData(username).subscribe(function (testData) { return _this.adata = testData; });
+    };
+    AppComponent.prototype.editData = function (i) {
+        this.update = true;
+        this.datatobeedited = i;
+        console.log("obj is" + this.datatobeedited.name);
+        // this._dataService.editData(name).subscribe((testData)=>this.test=testData)
+    };
+    AppComponent.prototype.saveUpdate = function (datatobesaved) {
+        var _this = this;
+        console.log("datatobesaved" + datatobesaved._id);
+        console.log("datatobesaved" + datatobesaved.username + '' +
+            ' ' + datatobesaved.address + ' ' + datatobesaved.email + ' ' + datatobesaved.contact);
+        var _url = "http://localhost:3000/api/data" + "/" + datatobesaved._id;
+        var headers = new __WEBPACK_IMPORTED_MODULE_3__angular_http__["a" /* Headers */]();
+        headers.append('Content-Type', 'application/json');
+        var options = new __WEBPACK_IMPORTED_MODULE_3__angular_http__["d" /* RequestOptions */]({ headers: headers });
+        this._http.put(_url, datatobesaved, options)
+            .subscribe(function (data) {
+            alert('saved Successfully');
+            _this.update = false;
+            // this.router.navigate(['/api/login']);
+        }, function (error) {
+            console.log(JSON.stringify(error.json()));
+        });
+    };
+    AppComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+            selector: 'app-root',
+            template: __webpack_require__("../../../../../src/app/app.component.html"),
+            styles: [__webpack_require__("../../../../../src/app/app.component.css")],
+            providers: [__WEBPACK_IMPORTED_MODULE_4__data_service__["a" /* DataService */]]
+        }),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_3__angular_http__["b" /* Http */], __WEBPACK_IMPORTED_MODULE_4__data_service__["a" /* DataService */]])
+    ], AppComponent);
+    return AppComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("../../../platform-browser/esm5/platform-browser.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__("../../../forms/esm5/forms.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_http__ = __webpack_require__("../../../http/esm5/http.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__addus_addus_component__ = __webpack_require__("../../../../../src/app/addus/addus.component.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+//import { RouterModule } from '@angular/router';
+
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["E" /* NgModule */])({
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */],
+                __WEBPACK_IMPORTED_MODULE_5__addus_addus_component__["a" /* AddusComponent */]
+            ],
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */], __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormsModule */], __WEBPACK_IMPORTED_MODULE_2__angular_forms__["b" /* ReactiveFormsModule */], __WEBPACK_IMPORTED_MODULE_3__angular_http__["c" /* HttpModule */]
+            ],
+            providers: [],
+            bootstrap: [__WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/data.service.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return DataService; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_http__ = __webpack_require__("../../../http/esm5/http.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_rxjs_add_operator_map__ = __webpack_require__("../../../../rxjs/_esm5/add/operator/map.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+var DataService = (function () {
+    function DataService(_http) {
+        this._http = _http;
+        this.url = "http://localhost:5000/api/java";
+        this.url2 = "http://localhost:5000/api/mean";
+        this.url3 = "http://localhost:3000/api/all";
+    }
+    DataService.prototype.getallData = function () {
+        return this._http.get(this.url3).map(function (res) { return res.json(); });
+    };
+    DataService.prototype.deleteData = function (username) {
+        return this._http.delete(this.url3 + "/" + username)
+            .map(function (response) { {
+            return response.json();
+        } });
+    };
+    DataService = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["w" /* Injectable */])(),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__angular_http__["b" /* Http */]])
+    ], DataService);
+    return DataService;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/environments/environment.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+var environment = {
+    production: false
+};
+
+
+/***/ }),
+
+/***/ "../../../../../src/main.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__("../../../platform-browser-dynamic/esm5/platform-browser-dynamic.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__("../../../../../src/app/app.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__("../../../../../src/environments/environment.ts");
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_7" /* enableProdMode */])();
+}
+Object(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */])
+    .catch(function (err) { return console.log(err); });
+
+
+/***/ }),
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("../../../../../src/main.ts");
+
+
+/***/ })
+
+},[0]);
+//# sourceMappingURL=main.bundle.js.map
